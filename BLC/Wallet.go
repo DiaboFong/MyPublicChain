ef7559492a8ba9350e41ca8dd8ff10a09c285dde
brucefeng <clovem.feng@gmail.com>
2018-08-16 09:24:30 +0800

    通过公钥生成钱包地址 	1. 原始公钥-->sha256 -->160 --->公钥Hash 	2. 版本号+公钥Hash -->校验码 	3. 版本号+公钥Hash + 校验码 -->Base58编码

--- BLC/Wallet.go
+++ BLC/Wallet.go
@@ -5,6 +5,8 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"log"
+	"crypto/sha256"
+	"golang.org/x/crypto/ripemd160"
 )
 
 //1.定义一个钱包结构:Wallet
@@ -14,7 +16,7 @@ type Wallet struct {
 	PrivateKey ecdsa.PrivateKey
 
 	//2.公钥
-	PublicKey []byte
+	PublicKey []byte //公钥
 }
 
 //2.产生一对密钥
@@ -52,4 +54,56 @@ func newKeyPair() (ecdsa.PrivateKey, []byte) {
 func NewWallet() *Wallet {
 	privateKey, publicKey := newKeyPair()
 	return &Wallet{privateKey, publicKey}
+}
+
+//定义版本号
+const version = byte(0x00)
+const addressCheckSumLen = 4
+
+//4.根据公钥获取对应的地址 https://www.blockchain.com/btc/address/
+//私钥 -- >公钥(原始公钥) -->sha256(hash) -->ripemd160(hash) -->hash值:公钥Hash :pubkeyHash
+func (w *Wallet) GetAdrress() []byte {
+	/*
+	1. 原始公钥-->sha256 -->160 --->公钥Hash
+	2. 版本号+公钥Hash -->校验码
+	3. 版本号+公钥Hash + 校验码 -->Base58编码
+	 */
+
+	//1.得到公钥Hash
+	pubKeyHash := PubKeyHash(w.PublicKey)
+	//2. 添加版本号: 比特币中为0
+	version_payload := append([]byte{version}, pubKeyHash...)
+	//3.根据versioned_payload -- >两次sha256，取前四位，得到checksum
+	checkSumByte := CheckSum(version_payload)
+	//4. 拼接所有的数据
+	full_payload := append(version_payload, checkSumByte...)
+	//5.Base58编码
+	address := Base58Encode(full_payload)
+
+	return address
+}
+
+//原始公钥-->公钥Hash
+// 1. sha256
+// 2. ripemd160
+func PubKeyHash(publicKey []byte) []byte {
+	//1.sha256
+	hasher256 := sha256.New()
+	hasher256.Write(publicKey)
+	hash256 := hasher256.Sum(nil)
+	//2.ripemd160
+	hasher160 := ripemd160.New()
+	hasher160.Write(hash256)
+	hash160 := hasher160.Sum(nil)
+	//返回
+	return hash160
+
+}
+
+//定义一个函数，用于产生校验码
+//进行两次sha256 Hash
+func CheckSum(payload []byte) []byte {
+	firstHash := sha256.Sum256(payload)
+	secondHash := sha256.Sum256(firstHash[:])
+	return secondHash[:addressCheckSumLen]
 }
--- main.go
+++ main.go
@@ -10,4 +10,8 @@ func main() {
 	wallet := BLC.NewWallet()
 	fmt.Println(wallet.PublicKey)
 	fmt.Println(wallet.PrivateKey)
+
+	address := wallet.GetAdrress()
+	fmt.Println(address)
+	fmt.Println(string(address))
 }
