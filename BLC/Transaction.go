beaaa9a8f2bc990333094e6b407329b0671442c7
brucefeng <clovem.feng@gmail.com>
2018-08-20 22:10:19 +0800

    调整

--- BLC/Transaction.go
+++ BLC/Transaction.go
@@ -76,7 +76,7 @@ func NewSimpleTransaction(from, to string, amount int64, utxoSet *UTXOSet, txs [
 
 
 	//获取钱包的集合：
-	_,wallets := GetWallets()
+	wallets := GetWallets()
 	wallet := wallets.WalletMap[from]
 
 	for txID, indexArray := range spentableUTXO {
